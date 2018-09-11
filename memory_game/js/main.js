d76802a10abe2df03dc4037db347aea44bf0ce40
Rafael Ruiz Bernal <rafa@rafaels-mbp.mynet>
2018-09-11 20:42:09 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,57 @@
 
+var cardsInPlay = [];
+var cards = [
+{
+rank: "queen", 
+suit: "hearts", 
+cardImage: "images/queen-of-hearts.png"
+}, 
+{
+rank: "queen", 
+suit: "diamonds", 
+cardImage: "images/queen-of-diamonds.png"
+}, 
+{
+rank: "king", 
+suit: "hearts", 
+cardImage: "images/king-of-hearts.png"
+}, 
+{
+rank: "king", 
+suit: "diamonds", 
+cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
+var checkForMatch = function() {
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else  {
+	alert("Sorry, try again");
+}
+};
+}
+
+var flipCard = function(cardId) {
+
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+checkForMatch();
+
+
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
+
+};
+
+flipCard(0);
+flipCard(2);
+
+/*OLD CODE: 
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
@@ -20,16 +73,12 @@ cardsInPlay.push(cards[cardId]);
 checkForMatch();
 
 
-
-
-
 };
 
 flipCard(0);
-flipCard(2);
+flipCard(2); 
 
-
-
+*/
 
 
 
